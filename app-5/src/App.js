31e42d52fe21362ffd5ed8ee44b3d1a49c5593c0
Spencer <sdvair@gmail.com>
2019-03-12 17:22:18 -0600

    completed app5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,20 +1,14 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Picture from './components/Picture'
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Picture pic={"https://news.nationalgeographic.com/content/dam/news/2016/09/08/humpback-whales/01humpbackwhales.ngsversion.1473420603711.adapt.1900.1.jpg"}/>
       </div>
-    );
+    )
   }
 }
 
---
+++ app-5/src/components/Picture.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Picture(props) {
+    return (
+        <div>
+            <img src={props.pic} width="100%"/>
+        </div>
+    )
+}
+
+export default Picture
